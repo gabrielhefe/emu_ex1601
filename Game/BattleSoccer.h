4d63a9586da2f99ed80abc4a2b3c4f17b8a8631b
Gabriel Ferreira <gabrielhefe@users.noreply.github.com>
2025-11-25 20:20:28 -0300

    Refactor BattleSoccer to direct member access

--- Game/BattleSoccer.h
+++ Game/BattleSoccer.h
@@ -3,88 +3,79 @@
 * Copyright (C) 2008-2017 Dimension Gamers <http://www.dimensiongamers.net>
 *
 * File: "BattleSoccer.h"
-*
 */
 #ifndef BATTLE_SOCCER_H
 #define BATTLE_SOCCER_H
 
 struct BattleSoccerPlayer
 {
-	DECLARE_PTR(Player, Player);
-	DECLARE_BOOL(Master);
+    Player* m_Player;
+    bool m_Master;
 
-	void Reset()
-	{
-		this->SetPlayer(nullptr);
-		this->SetMaster(false);
-	}
+    BattleSoccerPlayer(): m_Player(nullptr), m_Master(false) {}
+
+    void Reset()
+    {
+        this->m_Player = nullptr;
+        this->m_Master = false;
+    }
 };
 
 class BattleSoccerTeam
 {
-	public:
-		DECLARE_PTR(Guild, Guild);
-		DECLARE_PROPERTY(int32, Score);
-		DECLARE_ENUM(uint16, PartyID);
+    public:
+        BattleSoccerTeam();
 
-		void Reset()
-		{
-			this->SetGuild(nullptr);
-			this->SetScore(0);
-			this->SetPartyID(-1);
-			
-			PARTY_LOOP(i)
-			{
-				this->GetPlayer(i)->Reset();
-			}
-		}
+        void Reset();
 
-	public:
-		DECLARE_ARRAY_STRUCT(BattleSoccerPlayer, Player, MAX_PARTY_MEMBERS);
+        Guild* m_Guild;
+        int32 m_Score;
+        uint16 m_PartyID;
+        BattleSoccerPlayer m_Player[MAX_PARTY_MEMBERS];
 };
 
 class CBattleSoccerMgr
 {
-	SingletonInstance(CBattleSoccerMgr);
+    SingletonInstance(CBattleSoccerMgr);
 
-	public:
-		CBattleSoccerMgr();
-		virtual ~CBattleSoccerMgr();
+    public:
+        CBattleSoccerMgr();
+        virtual ~CBattleSoccerMgr();
 
-		void Update();
+        void Update();
 
-		void SetState_None();
-		void SetState_StandBy();
-		void SetState_Playing();
-		void SetState_Playend();
-		
-		void ProcState_None();
-		void ProcState_StandBy();
-		void ProcState_Playing();
-		void ProcState_Playend();
+        void SetState_None();
+        void SetState_StandBy();
+        void SetState_Playing();
+        void SetState_Playend();
 
-		void UpdateBall(Monster* Ball);
+        void ProcState_None();
+        void ProcState_StandBy();
+        void ProcState_Playing();
+        void ProcState_Playend();
 
-		bool IsInField(Player const* pPlayer) const;
-		bool GetRespawnLocation(int16 & x, int16 & y, Player* pPlayer);
-		bool IsAttackAllowed(Player* pPlayer01, Player* pPlayer02);
+        void UpdateBall(Monster* Ball);
 
-		void MovePlayersOut();
+        bool IsInField(Player const* pPlayer) const;
+        bool GetRespawnLocation(int16 & x, int16 & y, Player* pPlayer);
+        bool IsAttackAllowed(Player* pPlayer01, Player* pPlayer02);
 
-		void ProcessRequest(Player* pPlayer, const char * guild);
-		bool PartySuccess(Party* pParty, Guild* pGuild);
-		void ProcessTeams(Player* pPlayer01, Player* pPlayer02, Guild* pGuild01, Guild* pGuild02);
-		void SetTeamData(Guild* pGuild01, Guild* pGuild02);
+        void MovePlayersOut();
 
-		void Finish(uint8 reason, uint8 winner, uint8 looser, int32 score);
+        void ProcessRequest(Player* pPlayer, const char * guild);
+        bool PartySuccess(Party* pParty, Guild* pGuild);
+        void ProcessTeams(Player* pPlayer01, Player* pPlayer02, Guild* pGuild01, Guild* pGuild02);
+        void SetTeamData(Guild* pGuild01, Guild* pGuild02);
 
-		void SendScore();
-	private:
-		DECLARE_ARRAY_STRUCT(BattleSoccerTeam, Team, GUILD_WAR_TEAM_MAX);
-		DECLARE_ENUM(BattleSoccerState, State);
-		DECLARE_STRUCT(TickTimer, Time);
-		DECLARE_STRUCT(TickTimer, BallMoveTime);
-		DECLARE_BOOL(BallMove);
+        void Finish(uint8 reason, uint8 winner, uint8 looser, int32 score);
+
+        void SendScore();
+    public:
+        BattleSoccerTeam m_Team[GUILD_WAR_TEAM_MAX];
+        BattleSoccerState m_State;
+        TickTimer m_Time;
+        TickTimer m_BallMoveTime;
+        bool m_BallMove;
 };
 
 #endif
--- Game/ai_soccer_ball.cpp
+++ Game/ai_soccer_ball.cpp
@@ -6,46 +6,46 @@ public:
 
 	MonsterAI* GetAI(Monster* monster) const { return new SoccerBallAI(monster); }
 
-	struct SoccerBallAI: public MonsterAI
-	{
-		DECLARE_ENUM(BattleSoccerState, State);
+        struct SoccerBallAI: public MonsterAI
+        {
+                BattleSoccerState m_State;
 
-		explicit SoccerBallAI(Monster* monster): MonsterAI(monster)
-		{
-			this->SetState(BATTLE_SOCCER_STATE_NONE);
-			me()->SetRespawnLocation(MONSTER_RESPAWN_ZONE);
-		}
+                explicit SoccerBallAI(Monster* monster): MonsterAI(monster)
+                {
+                        this->m_State = BATTLE_SOCCER_STATE_NONE;
+                        me()->SetRespawnLocation(MONSTER_RESPAWN_ZONE);
+                }
 
 		virtual ~SoccerBallAI() {}
 
-		bool Update()
-		{
-			if ( this->GetState() != sBattleSoccerMgr->GetState() )
-			{
-				this->SetState(sBattleSoccerMgr->GetState());
+                bool Update()
+                {
+                        if ( this->m_State != sBattleSoccerMgr->m_State )
+                        {
+                                this->m_State = sBattleSoccerMgr->m_State;
 
-				if ( this->GetState() == BATTLE_SOCCER_STATE_STANDBY )
-				{
-					me()->Kill();
-				}
-			}
+                                if ( this->m_State == BATTLE_SOCCER_STATE_STANDBY )
+                                {
+                                        me()->Kill();
+                                }
+                        }
 
-			sBattleSoccerMgr->UpdateBall(me());
-			return true;
-		}
+                        sBattleSoccerMgr->UpdateBall(me());
+                        return true;
+                }
 
-		bool EnableAttack(Unit* pAttacker, Skill* pSkill, int32 count)
-		{
-			if ( sBattleSoccerMgr->GetState() == BATTLE_SOCCER_STATE_PLAYING && sBattleSoccerMgr->IsBallMove() && count <= 1 )
-			{
-				if ( pSkill )
-					pAttacker->PushBackCount(me(), 3);
-				else
-					pAttacker->PushBackCount(me(), 2);
-			}
+                bool EnableAttack(Unit* pAttacker, Skill* pSkill, int32 count)
+                {
+                        if ( sBattleSoccerMgr->m_State == BATTLE_SOCCER_STATE_PLAYING && sBattleSoccerMgr->m_BallMove && count <= 1 )
+                        {
+                                if ( pSkill )
+                                        pAttacker->PushBackCount(me(), 3);
+                                else
+                                        pAttacker->PushBackCount(me(), 2);
+                        }
 
-			if ( pSkill )
-				pAttacker->MagicAttackSend(ENTRY(me()), pSkill->GetSkill());
+                        if ( pSkill )
+                                pAttacker->MagicAttackSend(ENTRY(me()), pSkill->GetSkill());
 
 			return false;
 		}
